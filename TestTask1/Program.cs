b0e8cc4e759b9d813eac77199a53ee041377bd83
YuliaDr <strekozka_ulya@mail.ru>
2024-02-20 17:13:18 +0300

    Added Task1

---
+++ TestTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text.Json;
+using RestSharp;
+
+namespace TestTask1;
+
+internal class Program
+{
+    public static async Task Main()
+    {
+        var client = new RestClient("https://swapi.dev/api/");
+        var request = new RestRequest("people/1");
+        var response = await client.GetAsync(request);
+
+        if (response.StatusCode == HttpStatusCode.OK && response.Content != null)
+        {
+            // Настройки для (де)сериализации JSON ответа в SnakeCaseLower
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            var getPersonByIdResponse =
+                JsonSerializer.Deserialize<GetPersonByIdResponse>(response.Content, serializeOptions);
+
+            if (getPersonByIdResponse?.Films != null)
+            {
+                foreach (var film in getPersonByIdResponse.Films)
+                {
+                    Console.WriteLine(film);
+                }
+            }
+        }
+    }
+}
